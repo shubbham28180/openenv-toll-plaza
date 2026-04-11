907398092d87bbd0ac9ba8612fc49c7005e82264
Shubham Gupta <tumhara_github_email@gmail.com>
2026-04-11 19:08:37 +0530

    Final fix: Class reference without brackets

--- server/app.py
+++ server/app.py
@@ -1,14 +1,10 @@
 import uvicorn
 from openenv_core.env_server import create_app
-from .engine import TollPlazaEngine  # Ya TollEnv jo bhi aapka class name hai
+from .engine import TollPlazaEngine 
 from .models import Action, Observation
 
-# DHAYAN DEIN: Yahan bracket () nahi lagane hain
-# Galt: engine = TollPlazaEngine()
-# Sahi: engine = TollPlazaEngine
-
 app = create_app(
-    TollPlazaEngine,  # Sirf class ka naam, bracket nahi!
+    TollPlazaEngine,  
     Action, 
     Observation, 
     env_name="toll_plaza_env"
